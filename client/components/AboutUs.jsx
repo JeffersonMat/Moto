076996cb080f8632a1d658aa7e3c8ceeceafb02e
kaewkors <kaewkor.s@gmail.com>
2020-09-27 11:50:07 +1300

    created AboutUs

---
+++ client/components/AboutUs.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Booking() {
+ 
+    return (
+        <>
+            <h1>Coworking—simplified.</h1> 
+            <p>            
+                In response to a disruption in the way we think about work, 'mote connects remote workers with alternative work spaces.
+                <br/> Think Airbnb, but for remote workers.
+                
+                Pick a spare desk at a big wig like Xero for the week, or perch up at a local brewery for the day.
+                <br/>
+                'mote offers flexible coworking solutions for freelancers and destination coworkers who want the value of a sharedspace without the added commitment.
+            </p>
+            
+            <p>
+                Welcome to the future of work.
+            </p>
+        </>
+
+    )
+
+
+}
+
